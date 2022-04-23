ac67612854bc2b2178f5066dc876f8a6854454c0
XniceCraft <49149283+XniceCraft@users.noreply.github.com>
2022-04-23 23:44:27 +0700

    Statically include to program

--- utils/mediafire.py
+++
@@ -1,9 +0,0 @@
-#pylint: disable=missing-function-docstring,missing-module-docstring
-from lxml.html import fromstring as fr
-from utils.exceptions import DirectLinkError
-
-def get_direct_link(html):
-    try:
-        return fr(html).xpath('//a[@id="downloadButton"]/@href')[0]
-    except IndexError:
-        raise DirectLinkError from None
